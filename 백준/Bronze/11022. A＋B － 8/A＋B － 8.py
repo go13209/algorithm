25ed12b71af3e643521fa001cab84798218d886e
Rilee Kim <go13209@naver.com>
2023-01-16 11:46:06 +0900

    [Bronze V] Title: A+B - 8, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+for t in range(1, T + 1):
+    numbers = list(map(int, input().split()))
+    print(f'Case #{t}: {numbers[0]} + {numbers[1]} = {numbers[0] + numbers[1]}')
