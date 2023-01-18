635ebb9e24f3ac5b4d996e39599dc59e1dba6fc6
Rilee Kim <go13209@naver.com>
2023-01-18 11:58:19 +0900

    [Bronze II] Title: 문자열 반복, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,7 +1,7 @@
 T = int(input())
 for t in range(T):
-    new_str = ''
-    a, b = input().split()
+    a, b = map(str, input().split())
+    new_word = ''
     for char in b:
-        new_str += char * int(a)
-    print(new_str)
+        new_word += char * int(a)
+    print(new_word)
