a3cab861633207526cd3b594b82958ce9b6a258f
Rilee Kim <go13209@naver.com>
2023-01-18 17:52:57 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -3,5 +3,5 @@ str = input()
 cnt = 0
 for char in croatia:
     if char in str:
-        cnt += str.count(char)
-print(cnt + (len(str) - (cnt * 2)))
+        str = str.replace(char, '!')
+print(len(str))
