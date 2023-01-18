33bd2308167fddfa77a80f02b1be4a7056dfe1b2
Rilee Kim <go13209@naver.com>
2023-01-18 14:09:16 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,9 +1,6 @@
-alphabet_dict = {}
-for n in range(97, 123):
-    alphabet_dict[chr(n)] = n - 97
-input_list = list(map(str, input()))
-for a in alphabet_dict.keys():
-    if a in input_list:
-        print(input_list.index(a), end = ' ')
+S = input()
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) in S:
+        print(S.index(chr(i)), end=' ')
     else:
-        print(-1, end=' ')
+        print(-1, end = ' ')
