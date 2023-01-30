089efa0a8d0890941d3f3a6eadc63441de261901
Rilee Kim <go13209@naver.com>
2023-01-30 14:39:51 +0900

    [Bronze III] Title: 나는 요리사다, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
@@ -1,8 +1,13 @@
 scores = [list(map(int, input().split())) for _ in range(5)]
-highest_score = sum(scores[-1])
+# 요리사 다섯 명의 점수를 이중리스트의 형태로 입력받는다.
+highest_score = sum(scores[0])
 number = 1
+# 가장 높은 점수를 첫 번째 요리사의 점수로 임의 설정해놓는다.
 for score in scores:
-    if sum(score) >= highest_score:
+    if sum(score) > highest_score:
         highest_score = sum(score)
         number = 1 + scores.index(score)
+# for문을 돌려 첫 번째 요리사의 점수보다 높은 점수가 있을 경우 새롭게 highest_score 변수에 저장한다.
+# 오리사의 번호는 0이 아닌 1부터이므로 1을 더한다. 
 print(number, highest_score)
+# 우승자의 번호와 점수를 출력한다.
