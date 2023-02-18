a20cec0cc4c5f670d9cd698834956533d219a4e0
Rilee Kim <go13209@naver.com>
2023-02-18 22:16:34 +0900

    [level 2] Title: 전화번호 목록, Time: 93.83 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -3,6 +3,5 @@ def solution(phone_book):
     for i in range(len(phone_book) - 1):
         if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
             return False
-            break
     else:
         return True
