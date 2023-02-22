81bc3434b0c6ae27ecdc1fc0462ecc61b658cf6d
Rilee Kim <go13209@naver.com>
2023-02-23 01:34:51 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,22 @@
+import math
+
+def solution(progresses, speeds):
+    days = []
+    answer = []
+    
+    for i in range(len(progresses)):
+        take_time = math.ceil((100 - progresses[i]) / speeds[i])
+        
+        if i == 0:
+            days.append(take_time)
+        else:
+            if take_time <= days[0]:
+                days.append(take_time)
+            else:
+                answer.append(len(days))
+                days.clear()
+                days.append(take_time)
+    else:
+        answer.append(len(days))
+                
+    return answer
