5ae4eb92a2d50ce1ba6c82fb448f51b295f690fc
깅귤 <go13209@naver.com>
2024-02-25 17:31:32 +0900

    [Gold IV] Title: 약수의 합, Time: 284 ms, Memory: 126180 KB -BaekjoonHub

---
+++ 백준/Gold/17425. 약수의 합/약수의 합.py
@@ -0,0 +1,15 @@
+import sys
+
+MAX = 1000000
+sumOfDivisors = [0] * (MAX + 1)
+prefix_sum = [0] * (MAX + 1)
+for i in range(1, MAX + 1):
+    for j in range(i, MAX + 1, i):
+        sumOfDivisors[j] += i
+    prefix_sum[i] = prefix_sum[i - 1] + sumOfDivisors[i]    
+
+t = int(sys.stdin.readline().strip())
+
+for _ in range(t):
+    num = int(sys.stdin.readline().strip())
+    print(prefix_sum[num])
